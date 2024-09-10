406eded60fbd8f4b4910c6036e94058eb5e299c9
Faza Alhakim <fazafal04@gmail.com>
2024-09-10 19:23:48 +0700

    Fix scroll category

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getMenu } from "../../stores/slices/menuSlice";
@@ -7,53 +7,80 @@ import Navbar from "../../components/navbar";
 const Menu = () => {
   const dispatch = useDispatch();
   const menu = useSelector((state) => state.menu.menu);
-  const token = useSelector((state)=> state.login.token)
-  const token_type = useSelector((state)=> state.login.token_type)
+  const token = useSelector((state) => state.login.token);
+  const token_type = useSelector((state) => state.login.token_type);
 
   useEffect(() => {
     dispatch(getMenu(token, token_type));
   }, []);
 
+  const [catName, setCatName] = useState("Seasonal menu");
+
+  const categoryClicked = (cat_name) => {
+    setCatName(cat_name);
+  };
+
   return (
     <div className="w-[100%] relative">
-        <Navbar/>
-      <header className="text-center py-6 text-xl font-semibold">MENU</header>
-      <main>
-        <div className="overflow-x-auto hide-scrollbar">
+      <Navbar />
+      <div className="fixed top-0">
+        <header className=" bg-white text-center py-6 text-xl font-semibold ">
+          MENU
+        </header>
+        <div className="max-w-[100vw] md:max-w-[450px] bg-white overflow-x-auto hide-scrollbar">
           <div className="flex">
             {menu.categories
               ? menu.categories.map((category) => (
-                  <p
+                  <div
                     key={category.category_name}
-                    className="whitespace-nowrap mx-3 "
+                    className={`px-3 py-3 ${
+                      catName === category.category_name
+                        ? "border-b-4 border-black text-black"
+                        : "text-[#a5a5a5]"
+                    }`}
                   >
-                    {category.category_name}
-                  </p>
+                    <button
+                      onClick={() => categoryClicked(category.category_name)}
+                    >
+                      <a
+                        href={`#${category.category_name}`}
+                        className="whitespace-nowrap font-semibold"
+                      >
+                        {category.category_name}
+                      </a>
+                    </button>
+                  </div>
                 ))
               : ""}
           </div>
         </div>
+      </div>
+      <main className="mb-[80px] mt-[128px]">
         <section>
           {menu.categories
             ? menu.categories.map((category) => (
                 <div key={category.category_name}>
-                  <h1 className="text-lg font-semibold bg-[#e0e0e0] px-3 py-2">
+                  <h1
+                    id={category.category_name}
+                    className="text-lg font-semibold bg-[#e0e0e0] px-3 py-2"
+                  >
                     {category.category_name}
                   </h1>
                   {menu.categories &&
                     category.menu.map((m) => (
                       <div key={m.name} className="flex py-3">
                         <div className="basis-[25%]">
-                          <img
-                            src={m.photo}
-                            alt={m.name}
-                          />
+                          <img src={m.photo} alt={m.name} />
                         </div>
                         <div className="basis-[65%]">
                           <p className="font-semibold">{m.name}</p>
-                          <p className="text-xs text-[#a9a8a8] pe-3">{m.description}</p>
+                          <p className="text-xs text-[#a9a8a8] pe-3">
+                            {m.description}
+                          </p>
                         </div>
-                          <p className="font-semibold pe-3">{m.price.toLocaleString("id-ID")}</p>
+                        <p className="font-semibold pe-3">
+                          {m.price.toLocaleString("id-ID")}
+                        </p>
                       </div>
                     ))}
                 </div>
